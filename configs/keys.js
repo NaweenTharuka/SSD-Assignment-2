2ae6a57668440d8d8c00829b62679577c9cf994e
NaweenTharuka <61498921+NaweenTharuka@users.noreply.github.com>
2021-10-03 03:02:45 +0530

    feature-updated-drive-login

---
+++ configs/keys.js
@@ -0,0 +1,14 @@
+const KEYS = {
+    'googleOauth': {
+        'clientID': "879660144889-u29ckcave6dh3ntg2m5q52kvqrcc1399.apps.googleusercontent.com",
+        'clientSecret': "tXyICQ-SzAs5jScBeXC4L95r",
+        'callback': '/auth/google/redirect'
+    },
+    "session_key": "secret123"
+}
+
+module.exports = KEYS
+
+
+
+
